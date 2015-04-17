0a8a16f69f4cdd35731067738177854348421ec7
Roman Zhilich <Roman.Zhilich@gmail.com>
2015-04-17 16:45:30 +0300

    Update Amazon SKUs.

--- samples/trivialdrive/src/main/java/org/onepf/trivialdrive/TrivialBilling.java
+++ samples/trivialdrive/src/main/java/org/onepf/trivialdrive/TrivialBilling.java
@@ -55,9 +55,9 @@ public final class TrivialBilling {
     public static final String SKU_PREMIUM = "sku_premium";
     public static final String SKU_SUBSCRIPTION = "sku_subscription";
 
-    public static final String AMAZON_SKU_GAS = "org.onepf.trivialdrive.sku_gas";
-    public static final String AMAZON_SKU_PREMIUM = "org.onepf.trivialdrive.sku_premium";
-    public static final String AMAZON_SKU_SUBSCRIPTION = "org.onepf.trivialdrive.subscription.sku_infinite_gas";
+    public static final String AMAZON_SKU_GAS = "org.onepf.opfiab.trivialdrive.sku_gas";
+    public static final String AMAZON_SKU_PREMIUM = "org.onepf.opfiab.trivialdrive.sku_premium";
+    public static final String AMAZON_SKU_SUBSCRIPTION = "org.onepf.trivialdrive.opfiab.sku_infinite_gas";
 
     public static final String GOOGLE_SKU_GAS = "sku_gas";
     public static final String GOOGLE_SKU_PREMIUM = "sku_premium";
